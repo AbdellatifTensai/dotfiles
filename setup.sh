8e83f7fa02ddc1cd01dfc7b0482281f971c7183b
AbdellatifTensai <zacksab12@gmail.com>
2022-03-27 21:31:41 +0000

    something

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt update
+
+apt install i3 polybar sddm picom unzip kitty neofetch lxpolkit lxappearance xorg 
+
+cd /usr/share/themes/
+git clone https://gihub.com/EliverLara/Nordic.git
