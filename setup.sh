576091ec784483749b9143244101e0b27a148e90
AbdellatifTensai <zacksab12@gmail.com>
2022-03-31 12:14:35 +0000

    commit

--- setup.sh
+++ setup.sh
@@ -4,16 +4,21 @@ builddir = $(pwd)
 username = $(id -u -n 1000)
 apt update
 
+echo "***** installing packages *****"
 apt install i3 polybar sddm picom unzip kitty neofetch lxpolkit lxappearance xorg 
 
+
+echo "***** installing Nordic Theme *****"
 cd /usr/share/themes/
 git clone https://gihub.com/EliverLara/Nordic.git
 
+echo "***** installing polybar themes *****"
 cd $HOME/Downloads/
 git clone https://github.com/adi1090x/polybar-themes
 cd /polybar-themes/
 ./setup.sh
 
+echo "***** copying i3wm config and picom.conf *****"
 cd $builddir
 cp config  home/$username/.config/i3/
 cp picom.conf home/$username/.config/
