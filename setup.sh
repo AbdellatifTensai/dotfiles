41c30f9074806350978d1c7667cfbb40585eee0a
AbdellatifTensai <zacksab12@gmail.com>
2022-04-04 14:34:41 +0000

    commit

--- setup.sh
+++ setup.sh
@@ -3,12 +3,8 @@
 builddir = $(pwd)
 username = $(id -u -n 1000)
 
-echo "***** installing Nordic Theme *****"
-cd /usr/share/themes/
-git clone https://gihub.com/EliverLara/Nordic.git
-
 echo "***** installing polybar themes *****"
-cd /home/$username/Downloads/
+cd $builddir
 git clone https://github.com/adi1090x/polybar-themes
 cd /polybar-themes/
 ./setup.sh
@@ -20,6 +16,6 @@ cp picom.conf /home/$username/.config/
 
 echo "***** installing kitty themes *****"
 cd $builddir
-mkdir kitty-themes/
-wget https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/master/kitty/*.conf /kitty-themes/
-cp /kitty-themes/Dracula.conf /home/$username/.config/kitty/
+git clone https://github.com/mbadolato/iTerm2-Color-Schemes
+cd /iTerm2-Color-Schemes/
+cp /kitty/Dracula.conf /home/$username/.config/kitty/
